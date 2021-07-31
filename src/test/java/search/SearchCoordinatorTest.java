a1a1d5ba0c766bc6438f50db3a97439b9352ee32
jeremy.zhang <zhangyue9306@gmail.com>
2021-07-31 19:34:58 +0800

    add test

--- src/test/java/search/SearchCoordinatorTest.java
+++ src/test/java/search/SearchCoordinatorTest.java
@@ -1,4 +1,34 @@
+package search;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SearchCoordinatorTest {
-  
+    private static SearchCoordinator searchCoordinator;
+
+    @BeforeAll
+    public static void  Setup() {
+        searchCoordinator = new SearchCoordinator(null, null);
+    }
+
+    @Test
+    void TestSplitDocuments() {
+        List<String> documents = new ArrayList<>(Arrays.asList("doc1", "doc2", "doc3", "doc4", "doc5"));
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("doc1", "doc2"),
+                Arrays.asList("doc3", "doc4"),
+                Collections.singletonList("doc5"));
+        int numWorkers = 3;
+        List<List<String>> results = SearchCoordinator.splitDocuments(documents, numWorkers);
+        assertEquals(expected, results);
+    }
+
+
 }
